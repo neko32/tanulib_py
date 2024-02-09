a34d21ae32046a81d8392bcbf7a85f3e909e7cd3
neko64 <neko64@outlook.jp>
2024-02-08 22:48:15 -0500

    added mp pool

--- tanukilib/tlib/core/core.py
+++ tanukilib/tlib/core/core.py
@@ -4,6 +4,9 @@ from concurrent.futures import ProcessPoolExecutor
 def get_num_cpu() -> int:
     return multiprocessing.cpu_count()
 
+def exec_parallel_sync(max_num, f, *inputs):
+    with multiprocessing.Pool(max_num) as p:
+        return p.map(f, *inputs)
 
 def exec_parallel_async(max_num, f, *inputs):
     with ProcessPoolExecutor(max_num) as executor:
--- unittest/test_core.py
+++ unittest/test_core.py
@@ -8,7 +8,7 @@ def calc_with_wait_multi_params_out(p:Tuple[int, int]):
     r = gen_scalar_randint(1, 10, 1000)
     m, n = p
     print(f"this will take {r[0] / 1000} msecs..")
-    time.sleep(r[0] / 100)
+    time.sleep(r[0] / 1000)
     return m * n
 
 class CoreTest(TestCase):
@@ -19,8 +19,12 @@ class CoreTest(TestCase):
 
     def test_exec_parallel_async(self):
         ipt = [(x, x) for x in range(1, 6)]
-        print(ipt)
         for i, r in enumerate(exec_parallel_async(5, calc_with_wait_multi_params_out, ipt)):
+            self.assertEqual(r, ipt[i][0] * ipt[i][1])
+
+    def test_exec_parallel_sync(self):
+        ipt = [(x, x) for x in range(1, 6)]
+        for i, r in enumerate(exec_parallel_sync(5, calc_with_wait_multi_params_out, ipt)):
             self.assertEqual(r, ipt[i][0] * ipt[i][1])
 
 if __name__ == "__main__":
