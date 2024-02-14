d9d802b8cebbda20ed4e910990c642c83b385d54
neko64 <neko64@outlook.jp>
2024-02-14 01:19:48 -0500

    get specific datetime

--- tanukilib/tlib/dateutil/date_repr.py
+++ tanukilib/tlib/dateutil/date_repr.py
@@ -4,7 +4,8 @@ def cur_datetime_as_std_fmt_str(date_delimitor:str = '-',
                                 time_delimitor:str = ':',
                                 with_t:bool = True,
                                 with_tz:bool = False, 
-                                with_ssec:bool = False) -> str:
+                                with_ssec:bool = False,
+                                timezone:datetime.timezone = datetime.timezone.utc) -> str:
     fmt = f"%Y{date_delimitor}%m{date_delimitor}%d"
     if with_t:
         fmt += "T"
@@ -13,5 +14,26 @@ def cur_datetime_as_std_fmt_str(date_delimitor:str = '-',
         fmt += ".%f"
     if with_tz:
         fmt += "%z"
-    return datetime.datetime.now(datetime.timezone.utc).strftime(fmt)
+    return datetime.datetime.now(timezone).strftime(fmt)
 
+def get_datetime_as_std_fmt_str(year:int,
+                                month:int,
+                                day:int,
+                                hour:int,
+                                minute:int,
+                                sec:int,
+                                date_delimitor:str = '-',
+                                time_delimitor:str = ':',
+                                with_t:bool = True,
+                                with_tz:bool = False,
+                                with_ssec:bool = False,
+                                timezone:datetime.timezone = datetime.timezone.utc) -> str:
+    fmt = f"%Y{date_delimitor}%m{date_delimitor}%d"
+    if with_t:
+        fmt += "T"
+    fmt += f"%H{time_delimitor}%M{time_delimitor}%S"
+    if with_ssec:
+        fmt += ".%f"
+    if with_tz:
+        fmt += "%z"
+    return datetime.datetime(year, month, day, hour, minute, sec, tzinfo = timezone).strftime(fmt)
--- unittest/test_daterepr.py
+++ unittest/test_daterepr.py
@@ -1,5 +1,6 @@
 from unittest import TestCase, main
 from tlib.dateutil import *
+from zoneinfo import ZoneInfo
 
 class DateReprTest(TestCase):
 
@@ -19,6 +20,20 @@ class DateReprTest(TestCase):
         print(cur_date_dsep_tsep)
         cur_date_dsep_tsep_full = cur_datetime_as_std_fmt_str(date_delimitor = '/', time_delimitor = '_', with_tz = True, with_ssec = True)
         print(cur_date_dsep_tsep_full)
+        cur_date_jst = cur_datetime_as_std_fmt_str(date_delimitor = '/', time_delimitor = '_', with_tz = True, with_ssec = True, timezone = datetime.timezone(datetime.timedelta(hours = 9), name = "JST"))
+        print(cur_date_jst)
+
+    def test_get_datetime_as_std_fmt_str(self):
+        date_s = get_datetime_as_std_fmt_str(2020, 3, 17, 11, 28, 35,  '-', ':', True, True, False, ZoneInfo("America/New_York"))
+        date = datetime.datetime.strptime(date_s, "%Y-%m-%dT%H:%M:%S%z")
+        self.assertEqual(date.year, 2020)
+        self.assertEqual(date.month, 3)
+        self.assertEqual(date.day, 17)
+        self.assertEqual(date.hour, 11)
+        self.assertEqual(date.minute, 28)
+        self.assertEqual(date.second, 35)
+        
+
 
 
 if __name__ == "__main__":
