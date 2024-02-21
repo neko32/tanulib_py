673d0c5e497055b9112fd764c488c5bb658ff88b
neko64 <neko64@outlook.jp>
2024-02-20 23:23:16 -0500

    added sha2 256 hash

--- tanukilib/tlib/datautil/hash.py
+++ tanukilib/tlib/datautil/hash.py
@@ -1,20 +1,44 @@
 import hashlib
-from typing import Tuple
 
-def md5_from_str(s:str) -> Tuple[bytes, str]:
-    md5 = hashlib.md5()
-    md5.update(s.encode())
-    return (md5.digest(), md5.hexdigest())
 
-def md5_from_bytes(b:bytes) -> Tuple[bytes, str]:
-    md5 = hashlib.md5()
-    md5.update(b)
-    return (md5.digest(), md5.hexdigest())
+def md5_from_str(s: str) -> bytes:
+    md5 = hashlib.md5(s.encode())
+    return md5.digest()
+
+
+def md5_from_bytes(b: bytes) -> bytes:
+    md5 = hashlib.md5(b)
+    return md5.digest()
+
+
+def sha2_256_from_str(s: str) -> bytes:
+    sha2_256 = hashlib.sha256(s.encode())
+    return sha2_256.digest()
+
+
+def sha2_256_from_bytes(b: bytes) -> bytes:
+    sha2_256 = hashlib.sha256(b)
+    return sha2_256.digest()
+
+
+def sha2_256_from_file(fpath: str) -> bytes:
+    sha256 = hashlib.sha256()
+    with open(fpath, 'r') as fp:
+        buf = fp.read()
+    sha256.update(bytes(buf.encode()))
+    return sha256.digest()
+
 
 # [TODO] limit by size
-def md5_from_file(fpath:str) -> Tuple[bytes, str]:
+
+
+def md5_from_file(fpath: str) -> bytes:
     md5 = hashlib.md5()
     with open(fpath, 'r') as fp:
         buf = fp.read()
     md5.update(bytes(buf.encode()))
-    return (md5.digest(), md5.hexdigest())
+    return md5.digest()
+
+
+def to_hex(b: bytes) -> str:
+    return b.hex()
--- unittest/test_hash.py
+++ unittest/test_hash.py
@@ -1,30 +1,59 @@
 from unittest import TestCase, main
 from tlib.datautil.hash import *
 
+
 class HashTest(TestCase):
 
     def test_md5_from_str(self):
         ipt = "tako neko poko 37"
-        b_md5, hexs_md5 = md5_from_str(ipt)
+        b_md5 = md5_from_str(ipt)
+        hexs_md5 = to_hex(b_md5)
         self.assertEqual(hexs_md5, "2c7d0d52f027a23d974016f710c3b648")
         self.assertEqual(b_md5.hex(), hexs_md5)
-        
+
     def test_md5_from_byte(self):
-        ipt = "tako neko poko 37"
-        b_md5, hexs_md5 = md5_from_str(ipt)
+        ipt = b"tako neko poko 37"
+        b_md5 = md5_from_bytes(ipt)
+        hexs_md5 = to_hex(b_md5)
         self.assertEqual(hexs_md5, "2c7d0d52f027a23d974016f710c3b648")
         self.assertEqual(b_md5.hex(), hexs_md5)
 
     def test_md5_from_file(self):
         fpath = "./testdata/text/plain.txt"
-        b_md5, hexs_md5 = md5_from_file(fpath)
+        b_md5 = md5_from_file(fpath)
+        hexs_md5 = to_hex(b_md5)
         self.assertEqual(hexs_md5, "010f2f225d7e05ce8357dcd0736d8b76")
         self.assertEqual(b_md5.hex(), hexs_md5)
 
+    def test_sha2_256_from_str(self):
+        ipt = "tako neko poko 37"
+        b_sha256 = sha2_256_from_str(ipt)
+        hexs_sha256 = to_hex(b_sha256)
+        self.assertEqual(
+            hexs_sha256,
+            "1f366a9a660ac1c84f665defd37146861a55ee4a4b1e3af3e3d9b7b98f95d8ab"
+        )
+        self.assertEqual(b_sha256.hex(), hexs_sha256)
 
+    def test_sha2_256_from_byte(self):
+        ipt = b"tako neko poko 37"
+        b_sha256 = sha2_256_from_bytes(ipt)
+        hexs_sha256 = to_hex(b_sha256)
+        self.assertEqual(
+            hexs_sha256,
+            "1f366a9a660ac1c84f665defd37146861a55ee4a4b1e3af3e3d9b7b98f95d8ab"
+        )
+        self.assertEqual(b_sha256.hex(), hexs_sha256)
 
-
-    
+    def test_sha256_from_file(self):
+        fpath = "./testdata/text/plain.txt"
+        b_sha256 = sha2_256_from_file(fpath)
+        hexs_sha256 = to_hex(b_sha256)
+        self.assertEqual(
+            hexs_sha256,
+            "20a38d3ba13f9ba2331fbec53542ac7ef5800c880215db28be272da92753146b"
+        )
+        self.assertEqual(b_sha256.hex(), hexs_sha256)
 
 
 if __name__ == "__main__":
