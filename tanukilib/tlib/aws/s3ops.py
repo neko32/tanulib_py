7d881b342b6da7cce7d27692744373d3a279353d
neko64 <neko64@outlook.jp>
2024-02-20 22:42:17 -0500

    added list buckets

--- tanukilib/tlib/aws/s3ops.py
+++ tanukilib/tlib/aws/s3ops.py
@@ -1,8 +1,9 @@
 import boto3
 from enum import Enum, auto
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Tuple
 import os
-import json
+from datetime import datetime
+
 
 class ACLTypes(Enum):
     private = auto()
@@ -10,8 +11,9 @@ class ACLTypes(Enum):
     public_read_write = auto()
     authenticated_read = auto()
 
+
 class S3ObjectKeySize:
-    def __init__(self, key:str, size:int) -> None:
+    def __init__(self, key: str, size: int) -> None:
         self._key = key
         self._size = size
 
@@ -24,9 +26,21 @@ class S3ObjectKeySize:
         return self._size
 
 
+class S3BucketListResult:
+
+    def __init__(self, resp: Dict[Any, Any]) -> None:
+        self.json_data = resp
+
+    def get_list(self) -> List[Tuple[str, datetime]]:
+        result = []
+        for bucket in self.json_data["Buckets"]:
+            result.append((bucket["Name"], bucket["CreationDate"]))
+        return result
+
+
 class S3ObjectListResult:
-    
-    def __init__(self, resp:Dict[Any, Any]) -> None:
+
+    def __init__(self, resp: Dict[Any, Any]) -> None:
         self.json_data = resp
 
     def list_key_and_size(self) -> List[S3ObjectKeySize]:
@@ -36,7 +50,7 @@ class S3ObjectListResult:
         return result
 
 
-def _from_acltypes_to_str(acl:ACLTypes) -> str:
+def _from_acltypes_to_str(acl: ACLTypes) -> str:
     if acl == ACLTypes.private:
         return "private"
     elif acl == ACLTypes.public_read:
@@ -48,60 +62,67 @@ def _from_acltypes_to_str(acl:ACLTypes) -> str:
     else:
         raise NotImplementedError()
 
+
 def create_s3_client():
-    session = boto3.session.Session() 
+    session = boto3.session.Session()
     env = os.environ['ENV']
-    
+
     cl = session.client(
-        service_name = 's3',
-        endpoint_url = 'http://localhost:4566'
-    ) if env == "local_ut" else session.client(service_name = 's3')
+        service_name='s3',
+        endpoint_url='http://localhost:4566'
+    ) if env == "local_ut" else session.client(service_name='s3')
     return (session, cl)
 
-def create_s3_bucket(bucket_name:str,
-                    acl_type:ACLTypes,
-                    ) -> str:
+
+def create_s3_bucket(bucket_name: str,
+                     acl_type: ACLTypes,
+                     ) -> str:
     _, cl = create_s3_client()
 
     return cl.create_bucket(
-        ACL = _from_acltypes_to_str(acl_type),
-        Bucket = bucket_name,
+        ACL=_from_acltypes_to_str(acl_type),
+        Bucket=bucket_name,
     )
 
-def delete_s3_bucket(bucket_name:str) -> bool:
+
+def delete_s3_bucket(bucket_name: str) -> bool:
     try:
         _, cl = create_s3_client()
-        cl.delete_bucket(Bucket = bucket_name)
+        cl.delete_bucket(Bucket=bucket_name)
         return True
     except Exception as e:
         print(f"@delete_s3_bucket:error occurred - {e}")
         return False
 
-def put_object_to_s3_bucket(bucket_name:str, 
-                            acl:ACLTypes,
-                            key:str, 
-                            data:bytes) -> str:
+
+def put_object_to_s3_bucket(bucket_name: str,
+                            acl: ACLTypes,
+                            key: str,
+                            data: bytes) -> str:
     _, cl = create_s3_client()
     return cl.put_object(
-        Bucket = bucket_name,
-        Key = key,
-        ACL = _from_acltypes_to_str(acl),
-        Body = data
+        Bucket=bucket_name,
+        Key=key,
+        ACL=_from_acltypes_to_str(acl),
+        Body=data
     )
 
-def delete_object_from_s3_bucket(bucket_name:str, key:str) -> bool:
+
+def delete_object_from_s3_bucket(bucket_name: str, key: str) -> bool:
     try:
         _, cl = create_s3_client()
-        cl.delete_object(Bucket = bucket_name, Key = key)
+        cl.delete_object(Bucket=bucket_name, Key=key)
         return True
     except Exception as e:
         print(e)
         return False
 
 
-
-
-def list_object_in_s3_bucket(bucket_name:str) -> S3ObjectListResult:
+def list_buckets() -> S3BucketListResult:
     _, cl = create_s3_client()
-    return S3ObjectListResult(cl.list_objects_v2(Bucket = bucket_name))
-    
+    return S3BucketListResult(cl.list_buckets())
+
+
+def list_object_in_s3_bucket(bucket_name: str) -> S3ObjectListResult:
+    _, cl = create_s3_client()
+    return S3ObjectListResult(cl.list_objects_v2(Bucket=bucket_name))
--- unittest/test_awss3ops.py
+++ unittest/test_awss3ops.py
@@ -2,10 +2,12 @@ from unittest import TestCase, main
 from tlib.aws import s3ops
 import os
 
+
 class AWSS3OpsTest(TestCase):
 
     def setUp(self):
         self.bucket_name = "testbk1"
+        self.bucket_name2 = "testbk2"
         os.environ['ENV'] = 'local_ut'
 
     def tearDown(self) -> None:
@@ -17,19 +19,32 @@ class AWSS3OpsTest(TestCase):
         test_json1_key = "testsimple1"
         rez = s3ops.create_s3_bucket(self.bucket_name, s3ops.ACLTypes.private)
         print(rez)
-        
+
         with open(test_json1_fpath, 'r') as fp:
             test_json1_data = bytes(fp.read(), 'utf-16')
-        
-        put_rez = s3ops.put_object_to_s3_bucket(f"{self.bucket_name}", s3ops.ACLTypes.private, test_json1_key, test_json1_data)
+
+        put_rez = s3ops.put_object_to_s3_bucket(
+            f"{self.bucket_name}",
+            s3ops.ACLTypes.private,
+            test_json1_key, test_json1_data)
         print(put_rez)
 
         lsrez = s3ops.list_object_in_s3_bucket(f"{self.bucket_name}")
         for ks in lsrez.list_key_and_size():
             print(f"{ks.key}:{ks.size}")
-            self.assertTrue(s3ops.delete_object_from_s3_bucket(self.bucket_name, ks.key))
+            self.assertTrue(s3ops.delete_object_from_s3_bucket(
+                self.bucket_name, ks.key))
 
+    def test_list_s3_buckets(self):
+        s3ops.create_s3_bucket(self.bucket_name, s3ops.ACLTypes.private)
+        s3ops.create_s3_bucket(self.bucket_name2, s3ops.ACLTypes.private)
+        names = ["testbk1", "testbk2"]
 
+        for (name, dt) in s3ops.list_buckets().get_list():
+            self.assertTrue(name in names)
+            self.assertEqual(type(dt).__name__, "datetime")
+
+        self.assertTrue(s3ops.delete_s3_bucket(self.bucket_name2))
 
 
 if __name__ == '__main__':
