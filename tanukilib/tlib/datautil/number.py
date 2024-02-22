d6a0f0e7852f39160c04ea7a033e77a6def0d8ef
neko64 <neko64@outlook.jp>
2024-02-21 19:41:35 -0500

    added round truncate aka toward zero

--- tanukilib/tlib/datautil/number.py
+++ tanukilib/tlib/datautil/number.py
@@ -1,15 +1,32 @@
 import decimal
 
 
-def round_to_nearest_half_up(v:float, decimal_point:int) -> float:
+def _quote_decimal_str(decimal_point: int) -> int:
     if decimal_point <= 0:
-        raise Exception(f"invalid decimal point {decimal_point}, must be greater than 0")
-    
+        raise Exception(
+            f"invalid decimal point {decimal_point}, must be greater than 0")
+
     if decimal_point == 1:
-        decimal_str = '0'
+        return '0'
     else:
         decimal_str = '0.'
         for _ in range(1, decimal_point - 1):
             decimal_str += '0'
         decimal_str += '1'
-    return float(decimal.Decimal(str(v)).quantize(decimal.Decimal(decimal_str), decimal.ROUND_HALF_UP))
+        return decimal_str
+
+
+def round_to_nearest_half_up(v: float, decimal_point: int) -> float:
+    decimal_str = _quote_decimal_str(decimal_point)
+    return float(decimal.Decimal(str(v)).quantize(
+        decimal.Decimal(decimal_str),
+        decimal.ROUND_HALF_UP)
+    )
+
+
+def round_truncate(v: float, decimal_point: int) -> float:
+    decimal_str = _quote_decimal_str(decimal_point)
+    return float(decimal.Decimal(str(v)).quantize(
+        decimal.Decimal(decimal_str),
+        decimal.ROUND_DOWN)
+    )
--- unittest/test_number.py
+++ unittest/test_number.py
@@ -2,11 +2,12 @@ from tlib.datautil import *
 from unittest import TestCase, main
 import numpy as np
 
+
 class DataUtilTest(TestCase):
 
     def test_gen_scalar_randint(self):
         r = gen_scalar_randint(10000, 0, 100)
-        
+
         self.assertTrue(np.all(r >= 0) and np.all(r <= 100))
 
     def test_gen_rand_alnum_str(self):
@@ -14,7 +15,7 @@ class DataUtilTest(TestCase):
         outs = [gen_rand_alnum_str(n) for _ in range(10)]
         self.assertTrue(any([is_str_alnum(s) for s in outs]))
         self.assertTrue(all([len(s) == n for s in outs]))
-        
+
     def test_round_to_nearest_half_up(self):
         v = 250.7259
         with self.assertRaises(Exception):
@@ -28,6 +29,20 @@ class DataUtilTest(TestCase):
         self.assertEqual(round_to_nearest_half_up(nv, 1), -251)
         self.assertEqual(round_to_nearest_half_up(nv, 2), -250.7)
         self.assertEqual(round_to_nearest_half_up(nv, 3), -250.73)
+
+    def test_round_truncate(self):
+        v = 250.7259
+        with self.assertRaises(Exception):
+            round_truncate(v, 0)
+        self.assertEqual(round_truncate(v, 1), 250)
+        self.assertEqual(round_truncate(v, 2), 250.7)
+        self.assertEqual(round_truncate(v, 3), 250.72)
+        nv = -250.7259
+        with self.assertRaises(Exception):
+            round_truncate(nv, 0)
+        self.assertEqual(round_truncate(nv, 1), -250)
+        self.assertEqual(round_truncate(nv, 2), -250.7)
+        self.assertEqual(round_truncate(nv, 3), -250.72)
 
 
 if __name__ == "__main__":
