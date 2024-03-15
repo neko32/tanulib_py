c36e28f8b7cddcf7c896ebeecef6cc51d731016b
neko64 <neko64@outlook.jp>
2024-03-15 09:48:44 -0400

    aws module pdoc

--- tanukilib/tlib/aws/s3ops.py
+++ tanukilib/tlib/aws/s3ops.py
@@ -1,18 +1,20 @@
 import boto3
-from enum import Enum, auto
+from enum import Enum
 from typing import List, Dict, Any, Tuple
 import os
 from datetime import datetime
 
 
 class ACLTypes(Enum):
-    private = auto()
-    public_read = auto()
-    public_read_write = auto()
-    authenticated_read = auto()
+    private = "private"
+    public_read = "public-read"
+    public_read_write = "public-read-write"
+    authenticated_read = "authenticated-read"
 
 
 class S3ObjectKeySize:
+    """Struct which contains S3 object's key and size"""
+
     def __init__(self, key: str, size: int) -> None:
         self._key = key
         self._size = size
@@ -27,6 +29,7 @@ class S3ObjectKeySize:
 
 
 class S3BucketListResult:
+    """Result data holder for get bucket list ops"""
 
     def __init__(self, resp: Dict[Any, Any]) -> None:
         self.json_data = resp
@@ -39,31 +42,24 @@ class S3BucketListResult:
 
 
 class S3ObjectListResult:
+    """Result data holder for list object ops"""
 
     def __init__(self, resp: Dict[Any, Any]) -> None:
         self.json_data = resp
 
     def list_key_and_size(self) -> List[S3ObjectKeySize]:
+        """returns list of each object's key and size"""
         result = []
         for content in self.json_data["Contents"]:
             result.append(S3ObjectKeySize(content["Key"], content["Size"]))
         return result
 
 
-def _from_acltypes_to_str(acl: ACLTypes) -> str:
-    if acl == ACLTypes.private:
-        return "private"
-    elif acl == ACLTypes.public_read:
-        return "public-read"
-    elif acl == ACLTypes.public_read_write:
-        return "public-read-write"
-    elif acl == ACLTypes.authenticated_read:
-        return "authenticated-read"
-    else:
-        raise NotImplementedError()
-
-
 def create_s3_client():
+    """
+    create S3 client.
+    Only if env is local_ut, then client for localstack is returned.
+    """
     session = boto3.session.Session()
     env = os.environ['ENV']
 
@@ -74,18 +70,21 @@ def create_s3_client():
     return (session, cl)
 
 
-def create_s3_bucket(bucket_name: str,
-                     acl_type: ACLTypes,
-                     ) -> str:
+def create_s3_bucket(
+        bucket_name: str,
+        acl_type: ACLTypes,
+) -> str:
+    """create a S3 bucket"""
     _, cl = create_s3_client()
 
     return cl.create_bucket(
-        ACL=_from_acltypes_to_str(acl_type),
+        ACL=acl_type.value,
         Bucket=bucket_name,
     )
 
 
 def delete_s3_bucket(bucket_name: str) -> bool:
+    """delete a s3 bucket"""
     try:
         _, cl = create_s3_client()
         cl.delete_bucket(Bucket=bucket_name)
@@ -99,16 +98,18 @@ def put_object_to_s3_bucket(bucket_name: str,
                             acl: ACLTypes,
                             key: str,
                             data: bytes) -> str:
+    """persist a data with key to the specified S3 bucket"""
     _, cl = create_s3_client()
     return cl.put_object(
         Bucket=bucket_name,
         Key=key,
-        ACL=_from_acltypes_to_str(acl),
+        ACL=acl.value,
         Body=data
     )
 
 
 def delete_object_from_s3_bucket(bucket_name: str, key: str) -> bool:
+    """delete an object by the specified key in the specified bucket"""
     try:
         _, cl = create_s3_client()
         cl.delete_object(Bucket=bucket_name, Key=key)
@@ -119,10 +120,12 @@ def delete_object_from_s3_bucket(bucket_name: str, key: str) -> bool:
 
 
 def list_buckets() -> S3BucketListResult:
+    """List s3 bucket"""
     _, cl = create_s3_client()
     return S3BucketListResult(cl.list_buckets())
 
 
 def list_object_in_s3_bucket(bucket_name: str) -> S3ObjectListResult:
+    """List all objects in the bucket specified"""
     _, cl = create_s3_client()
     return S3ObjectListResult(cl.list_objects_v2(Bucket=bucket_name))
