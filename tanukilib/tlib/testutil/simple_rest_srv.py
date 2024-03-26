bbaa23f35feb6264ae00db6e8ff82800777a08f2
neko64 <neko64@outlook.jp>
2024-03-25 21:43:58 -0400

    testutil pdoc

--- tanukilib/tlib/testutil/simple_rest_srv.py
+++ tanukilib/tlib/testutil/simple_rest_srv.py
@@ -3,22 +3,43 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 import json
 
+
 class CatInfo:
+    """
+    Cat Info used by simple http server sample
+    """
+
     def __init__(self, name, pattern):
         self.name = name
         self.pattern = pattern
 
     def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys = True, indent = 4, ensure_ascii = False)
+        """dumps attributes as json format"""
+        return json.dumps(
+            self,
+            default=lambda o: o.__dict__,
+            sort_keys=True,
+            indent=4,
+            ensure_ascii=False
+        )
 
-store = {"Shima": CatInfo("Shima", "Tabby"), "Torachan": CatInfo("Tora", "orangetabby")}
+
+store = {
+    "Shima": CatInfo("Shima", "Tabby"),
+    "Torachan": CatInfo("Tora", "orangetabby")
+}
+
 
 class SimpleRestServer(BaseHTTPRequestHandler):
+    """
+    Simple Rest Server sample which support only GET ops for cat info
+    """
 
     def do_GET(self):
+        """get cat information"""
 
         if self.path.startswith("/neko"):
-            # [TODO] add try except 
+            # [TODO] add try except
             print(self.path)
             parsed_url = urlparse(self.path)
             queries = parse_qs(parsed_url.query)
@@ -45,5 +66,7 @@ class SimpleRestServer(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(bytes("{}", "utf-8"))
 
+
 def build_simple_rest_srv() -> HTTPServer:
+    """Build a sample simple REST server with hard coded port 8030"""
     return HTTPServer(("localhost", 8030), SimpleRestServer)
