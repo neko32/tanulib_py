ca73fbf7efc9f3881c6aa856859855b5ae771097
neko64 <neko64@outlook.jp>
2024-03-26 02:12:42 -0400

    added login form

--- sample/ml/catdog_ui/views.py
+++ sample/ml/catdog_ui/views.py
@@ -1,15 +1,30 @@
 from catdog_ui import app
-from flask import render_template
+from flask import render_template, request, redirect
+
 
 @app.route('/')
 def show_entries():
     return render_template("entries/index.html")
 
+
 @app.route("/healthcheck")
 def show_healthcheck():
     return render_template("healthcheck/index.html")
 
-@app.route("/login")
-def show_login():
-    return render_template("login/index.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def show_login():
+
+    if request.method == "POST":
+        if request.form["username"] != app.config["USERNAME"]:
+            print("wrong user name")
+        elif request.form["password"] != app.config["PASSWORD"]:
+            print("wrong password")
+        else:
+            return redirect("/")
+    return render_template("login/index.html")
+        
+
+@app.route("/logout")
+def show_logout():
+    return redirect("/")
