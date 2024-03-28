cf2c20f72a86cd3282f26b96ac321cc4305afb4e
neko64 <neko64@outlook.jp>
2024-03-28 00:27:08 -0400

    math pdoc

--- tanukilib/tlib/math/math.py
+++ tanukilib/tlib/math/math.py
@@ -3,6 +3,9 @@ from math import gcd
 
 
 def factorial(n: int) -> int:
+    """
+    Calculate factorial
+    """
     if n < 0:
         raise ValueError("msut be positive")
     elif n == 0 or n == 1:
@@ -12,6 +15,7 @@ def factorial(n: int) -> int:
 
 
 def calc_npr(n: int, r: int) -> int:
+    """Calculate permutation nPr"""
     if n < r:
         raise ValueError(f"{n} must be bigger than or equal to {r}")
     sumv = 1
@@ -23,14 +27,17 @@ def calc_npr(n: int, r: int) -> int:
 
 
 def calc_ncr(n: int, r: int) -> int:
+    """Calculate combination nCr"""
     numerator = calc_npr(n, r)
     denominator = factorial(r)
     return numerator // denominator
 
 
 def sigmoid(x: float) -> float:
+    """calculate sigmoid x"""
     return 1 / (1. + np.exp(-x))
 
 
 def is_coprime(m: int, n: int) -> bool:
+    """checks whether m and n are co-prime or not"""
     return gcd(m, n) == 1
