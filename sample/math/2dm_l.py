824d104a2eb7a5fa750afa22ac0217ed7ac96888
neko64 <neko64@outlook.jp>
2024-04-03 23:27:51 -0400

    added inv,det and rank

--- sample/math/2dm_l.py
+++ sample/math/2dm_l.py
@@ -1,14 +1,22 @@
 import numpy as np
+import numpy.linalg as LA
 from tlib.math.la import *
 
 
 def main():
 
     m = np.array([
-        [0, -1],
-        [1, 0]
+        [1, 2],
+        [2, 4]
     ])
-    loc = np.array([[2], [1]])
+    loc = np.array([[0], [0]])
+    print(f"DET - {LA.det(m)}")
+    print(f"RANK - {LA.matrix_rank(m)}")
+    print("INV - ")
+    try:
+        print(LA.inv(m))
+    except:
+        print("N/A")
     lt_m_with_scaler_2d(
         m,
         loc,
@@ -16,6 +24,7 @@ def main():
         gen_graph=True,
         show_graph=True
     )
+
 
 
 if __name__ == "__main__":
