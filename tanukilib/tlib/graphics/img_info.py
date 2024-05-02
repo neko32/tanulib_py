b50f634acd2994592b47e6194d08534ea3701652
neko64 <neko64@outlook.jp>
2024-05-02 09:46:45 -0400

    aspect ratio

--- tanukilib/tlib/graphics/img_info.py
+++ tanukilib/tlib/graphics/img_info.py
@@ -1,6 +1,8 @@
 from tlib.core import exec_cmd
-from typing import Dict
+from typing import Dict, Tuple
+from cv2.typing import MatLike
 from pathlib import Path
+import math
 
 
 def get_exif_data(file_path: str) -> Dict[str, str]:
@@ -22,3 +24,25 @@ def get_exif_data(file_path: str) -> Dict[str, str]:
         v = v.strip()
         exif_dict[k] = v
     return exif_dict
+
+
+def aspect_ratio(w: float, h: float) -> Tuple[float, float]:
+    """calculate aspect ratio of provided width and height"""
+    gcd = math.gcd(int(w), int(h))
+    return (w / gcd, h / gcd)
+
+
+def aspect_ratio_of_image(img: MatLike) -> Tuple[float, float]:
+    """calculate aspect ratio of provided image"""
+    h, w = img.shape[:2]
+    gcd = math.gcd(int(w), int(h))
+    return (w / gcd, h / gcd)
+
+
+def is_horizontal_image_by_aspect_ratio(img: MatLike) -> bool:
+    """
+    Check whether the provided image is horizontal image or vertical one.
+    If aspect ratio is > 1, then assume it's horizontal. Else vertical.
+    """
+    ar = aspect_ratio_of_image(img)
+    return ar[0] / ar[1] > 1
--- unittest/test_imginfo.py
+++ unittest/test_imginfo.py
@@ -1,5 +1,6 @@
 from unittest import TestCase, main
 from tlib.graphics.img_info import *
+from tlib.graphics.graphics import imread_wrapper
 from pathlib import Path
 
 
@@ -16,9 +17,24 @@ class ImgInfoTest(TestCase):
 
     def test_nofileexist(self):
 
-        testimg_path = "/tako/tako.png"
+        testimg_path = str(Path(__file__).parent.joinpath(
+            "testdata", "img", "cat_img100000.jpg"))
         with self.assertRaises(Exception):
             get_exif_data(testimg_path)
+
+    def test_aspect_ratio(self):
+
+        self.assertTupleEqual(aspect_ratio(972., 560.), (243., 140.))
+        testimg_path = str(Path(__file__).parent.joinpath(
+            "testdata", "img", "nekovation640480.png"))
+        img = imread_wrapper(testimg_path)
+        self.assertTupleEqual(aspect_ratio_of_image(img), (4, 3))
+        self.assertTrue(is_horizontal_image_by_aspect_ratio(img))
+        testimg_path2 = str(Path(__file__).parent.joinpath(
+            "testdata", "img", "broken_img.jpg"))
+        img2 = imread_wrapper(testimg_path2)
+        self.assertTupleEqual(aspect_ratio_of_image(img2), (14., 15.))
+        self.assertFalse(is_horizontal_image_by_aspect_ratio(img2))
 
 
 if __name__ == "__main__":
