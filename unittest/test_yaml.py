d8777d6ac14101177f779a4f3beef155daf41b41
neko64 <neko64@outlook.jp>
2024-05-10 01:36:56 -0400

    added assertions to yaml test

--- unittest/test_yaml.py
+++ unittest/test_yaml.py
@@ -2,6 +2,7 @@ from unittest import TestCase, main
 from tlib.datautil.yaml import *
 from tlib.testutil.file import get_tmp_dir
 from pathlib import Path
+from csv import reader
 
 
 class YamlTest(TestCase):
@@ -15,6 +16,29 @@ class YamlTest(TestCase):
             tmp_yaml.unlink()
 
         to_csv(yaml_path, str(tmp_yaml), True)
+
+        with open(str(tmp_yaml), "r") as fd:
+            csvr = reader(fd)
+            cnt = 0
+            headers = [
+                'key', 'title', 'author_name',
+                'author_age', 'year', 'pages', 'chapters'
+            ]
+            for row in csvr:
+                if cnt == 0:
+                    self.assertListEqual(row, headers)
+                if cnt == 1:
+                    self.assertEqual(row[1], "Introduction to YAML")
+                    self.assertEqual(row[3], '38')
+                if cnt == 2:
+                    self.assertEqual(row[1], "Tako to Neko")
+                    self.assertEqual(row[3], '50')
+                cnt += 1
+
+            self.assertEqual(cnt, 3)
+
+        if tmp_yaml.exists():
+            tmp_yaml.unlink()
 
 
 if __name__ == "__main__":
